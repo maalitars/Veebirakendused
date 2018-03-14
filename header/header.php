59af1f8308a3bbbceafc12c88f0d15c248c77e13
DESKTOP-HH4A71M\Age Roosi <age.roosi@hotmail.com>
2018-03-14 12:12:03 +0200

    proov
    proov

--- header/header.php
+++ header/header.php
@@ -1,8 +1,22 @@
-<?php
 <head>
-    <title>"tere.tere"</title>
+    <title>SeenItAll</title>
+    <link rel = "stylesheet" href="style.css" type="text/css"/>
+    <style>
+
+
+    </style>
 </head>
 <body>
-	<body style="background-color:orange">
-  echo "HEA"
+  <div class="header">
+    <div id="logo">
+      <h1>SeenItAll</h1>
+    </div>
+    <div class="navigation_bar">
+      ESILEHT
+      MINU ANDMED
+      FILMID
+      LOGI VÄLJA
+    </div>
+    </div>
+  </div>
 </body>
