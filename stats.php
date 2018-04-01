377d2ecac1df2a5d83b70f9b2841733a21013d5a
Ludvig Leis <ludvigleis@gmail.com>
2018-04-01 20:49:43 +0300

    statistika kogumine

--- stats.php
+++ stats.php
@@ -5,7 +5,7 @@
     echo "<h3>Lehe külastajate statistika</h3>";
     echo "<table border=1 width=60%><tr><td colspan=4><t>Lehe külastajate statistika: </t> ";
 
-    $sql = "SELECT thedate_visited FROM stattracker ORDER BY id LIMIT 1";
+    $sql = "SELECT thedate_visited FROM statTracker ORDER BY id LIMIT 1";
     $result = $mysqli->query($sql);
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
@@ -15,14 +15,14 @@
     }}
     echo " <b>kuni:</b> $now</td></tr>";
 
-    $result1 = $mysqli->query ("SELECT COUNT(*) AS total FROM stattracker;");
+    $result1 = $mysqli->query ("SELECT COUNT(*) AS total FROM statTracker;");
     $row1 = mysqli_fetch_assoc($result1);
     $overall_total = $row1 ["total"];
     echo "<tr><td colspan=4><b>Mitu korda on lehte külastatud: </b>$overall_total</td></tr>";
 
     echo "<tr><td><b>Kuupäev</b></td><td><b>Aeg</b></td><td><b>IP aadress</b></td><td><b>Veebilehitseja</b></td></tr>";
 
-    $sql = "SELECT * FROM stattracker";
+    $sql = "SELECT * FROM statTracker";
     $result2 = $mysqli->query ($sql);
     while ($row2 = $result2->fetch_assoc()){
         $ip = $row2["ip"];
